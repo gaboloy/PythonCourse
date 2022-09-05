630e10f6dc7ec48040776ddaa383f0343750372c
gaboloy <gaboloy>
2022-09-05 20:09:10 +0000

    sesión numpy
    ejercicios trabajados durante la clase del 5 de sep

---
+++ Numpy Session (1).py
@@ -0,0 +1,32 @@
+# Databricks notebook source
+!pip install numpy
+
+import numpy as np
+
+list = [1,2,3]
+
+print (list)
+
+type (list)
+
+f=np.arange (3,34)
+
+f
+
+array_2=(0,1,2,3,4,0,1,2,3,4)
+
+array_2
+
+arr = np.append(f,array_2)
+
+arr
+
+arr.sort()
+
+arr
+
+matrix = np.arange (0,9).reshape(3,3)
+matrix
+
+id1 = np.identity(6)
+id1
---
+++ Python Course (1).py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+tupla = (11, "Pizza", False, "H20.ai", 39.4560)
+
+print (tupla)
+
+lista = []
+
+lista.append(tupla[0])
+lista.append(tupla[1])
+lista.append(tupla[2])
+lista.append(tupla[3])
+lista.append(tupla[4])
+
+print (lista)
+
+diccionario = {}
+
+diccionario[1]=lista[0]
+diccionario[2]=lista[1]
+diccionario[3]=lista[2]
+diccionario[4]=lista[3]
+diccionario[5]=lista[4]
+
+diccionario 
