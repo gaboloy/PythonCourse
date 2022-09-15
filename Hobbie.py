f9d44a744c05af1d99d6b8f3bec33dee50a450b9
gaboloy <gaboloy>
2022-09-15 20:36:14 +0000

    actividad hobby

---
+++ Hobbie.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+#Preparar pasta con tomate y trufa
+
+#1. Cocer la pasta aldente
+
+#2. Abrir bote de salsa de tomate con albahaca
+
+#3. En una sarten freir cebolla y tocino
+
+#4. Mezclar la salsa de tomate con el sofrito
+
+#5. Abrir bote con sala de trufa blanca
+
+#6. Verter en la salsa de trufa con sofrito
+
+#7. Verter salsa caliente sobre la pasta previamente colada
+
+#8. Servir una porción en un plato
+
+#9. Colocar queso parmesano encima
+
+#FIN#
